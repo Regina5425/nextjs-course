2111a300ce83fb30f95a6c81b35ce8d7c659ff01
Regina <reina5425@gmail.com>
2023-04-30 17:36:01 +0300

    update methods

---
+++ pages/api/data/socials.js
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    id: 3,
+    icon: "linkedin",
+    path: "http://www.linkedin.com/in/regina-anfenogentova-1a7104244",
+  },
+  {
+    id: 4,
+    icon: "telegram",
+    path: "https://t.me/ReginaAn",
+  },
+  {
+    id: 5,
+    icon: "github",
+    path: "https://github.com/Regina5425",
+  },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,34 @@
 import Head from "next/head";
 import Heading from "@/components/Heading";
 import styles from "../styles/Home.module.scss";
+import Socials from "@/components/Socials";
 
-const Home = () => {
+export const getStaticProps = async () => {
+  const response = await fetch(`${process.env.API_HOST}/socials/`);
+  const data = await response.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { socials: data },
+  };
+};
+
+const Home = ({ socials }) => {
   return (
-    <>
+    <div className={styles.wrapper}>
       <Head>
         <title>Home</title>
       </Head>
-      <div className={styles.wrapper}>
-        <Heading text='Home Page' />
+      <div className={styles.container}>
+        <Heading text='Next.js Application' />
+        <Socials socials={socials} />
       </div>
-    </>
+    </div>
   );
 };
 
